7494b0c16016faec19a5c8c897c5362d71d3b123
Jakub TBone <jakub.tbone@gmail.com>
2021-04-04 23:11:53 +0200

    Squashed commit of the following:
    commit 671647b74e9b7d60393417c14e61e89b1c01b665
    Author: Jakub TBone <jakub.tbone@gmail.com>
    Date:   Sun Apr 4 23:01:54 2021 +0200
    
        Browserslist, snowpack config, tsconfig, bump verions

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,3 +1,6 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
@@ -11,17 +14,26 @@ module.exports = {
     '@snowpack/plugin-postcss',
     '@snowpack/plugin-svelte',
   ],
-  packageOptions: {
-    polyfillNode: true,
-    // source: 'remote',
-    // types: true,
-  },
   devOptions: {
     port: 3000,
     open: 'none',
+  },
+  buildOptions: {
+    /* ... */
+  },
+  packageOptions: {
+    polyfillNode: true,
+    // types: true,
+    // source: 'remote',
   },
   alias: {
     // types: './types',
     components: './src/components',
   },
+  exclude: [
+    '**/node_modules/**/*',
+    '**/stories/**/*',
+    '**/*.stories.js',
+    '**/setupTests.(js|ts)',
+  ],
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import App from './components/App.svelte'
 
-import './assets/styles/main.scss'
+import './assets/styles/index.scss'
 
 const { SNOWPACK_PUBLIC_TEST } = import.meta.env
 console.log('this is env var test: ', SNOWPACK_PUBLIC_TEST)
