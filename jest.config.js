bdac6f68fca6ae47e35c9d9b5d0f73f182e09e8a
Jakub TBone <jakub.tbone@gmail.com>
2021-04-10 14:55:05 +0200

    Jest es6  with babel, type decs, bump versions

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,11 @@ module.exports = {
       },
     ],
     '^.+\\.ts$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest',
   },
   moduleFileExtensions: ['js', 'ts', 'svelte'],
   setupFilesAfterEnv: ['./src/setupTests.ts'],
+  moduleNameMapper: {
+    '^.+\\.(css|less|scss)$': 'identity-obj-proxy',
+  },
 }
