9ec3acb656e16c8c7849f816885934bb84ab85a8
Jakub TBone <jakub.tbone@gmail.com>
2021-04-23 21:09:23 +0200

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import Router, { RouteProps } from 'preact-router'
+import AsyncRoute from 'preact-async-route'
 import { h } from 'preact'
 import type { FunctionComponent } from 'preact'
 
@@ -5,10 +7,47 @@ import AppContextProvider from './context/AppContext'
 
 import Counter from 'components/Counter'
 
+/** demo header nav+search */
+const Home: FunctionComponent<{
+  matches?: any
+  path?: string
+  url?: string
+}> = ({ matches, path, url }) => {
+  return (
+    <div>
+      <Counter />
+    </div>
+  )
+}
+
+/** demo header nav+search */
+const Header = () => (
+  <header>
+    <nav>
+      <a href="/">Home</a>
+      <a href="/home">Home2</a>
+    </nav>
+  </header>
+)
+
+/** fall-back route (handles unroutable URLs) */
+const Error = ({ type, url }: any) => (
+  <section class="error">
+    <h2>Error {type}</h2>
+    <p>It looks like we hit a snag.</p>
+    <pre>{url}</pre>
+  </section>
+)
+
 const App: FunctionComponent = () => {
   return (
     <AppContextProvider>
-      <Counter />
+      <Header />
+      <Router>
+        <Home path="/" />
+        <Home path="/home" />
+        <Error type="404" default />
+      </Router>
     </AppContextProvider>
   )
 }
